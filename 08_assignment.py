dc381e52b60defa0a3a2268482275b002ce4385b
shilpa-rathod882 <shilpab882@gmail.com>
2022-08-27 17:28:53 +0530

    intger value

---
+++ 08_assignment.py
@@ -0,0 +1,45 @@
+# assignment problem 1
+x = 5
+y = 10
+print("hi")
+# print(c)
+
+# assignment problem 2
+a = 1
+b = 10
+print(a + b)
+
+a = 2
+print(a + b)
+
+p, q = 2, 3
+print(p, q)
+
+# r, q = p, r + 1
+# print(r, q)
+
+# assignment problem 3 ( Add two numbers )
+num1 = 20
+num2 = 25
+result = num1 + num2
+print("addtion : ", result)
+#assignment problem 4 (subtract two numbers)
+m = 65
+s = 85
+subtract = s-m
+print("sub : ", subtract)
+
+# assignment problem 5 (division of two numbers)
+
+g = 5
+u = 2
+divide = g/u
+print("div : ", divide)
+
+g1 = 5
+u1 = 2
+divide = g1%u1
+print("mod : ", divide)
+
+
+
