c40a3209fddea15d753add5710454628938609ce
shilpa-rathod882 <shilpab882@gmail.com>
2022-09-03 14:35:36 +0530

    forms

---
+++ 21_if_elif_else.py
@@ -0,0 +1,15 @@
+if(False):
+    print("This is if block")
+elif(True):
+    print("This is elif block")
+else:
+    print("this is else block")
+
+a = 5
+b = 5
+if(a>b):
+    print("a is grater than b")
+elif(a<b):
+  print("a is less than b")
+else:
+  print("a is equal to b")
