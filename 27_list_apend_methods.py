53f44b1477d22c06cc85cced9def4d1c6492ef87
shilpa-rathod882 <shilpab882@gmail.com>
2022-09-03 19:08:24 +0530

    function

---
+++ 27_list_apend_methods.py
@@ -0,0 +1,20 @@
+name = ["shilpa","chetan"]
+name.append("rathod")
+print(name)
+
+
+
+name = ["hello","rathod", "o"]
+print(name[0].count("o"))
+
+def add(a,b):
+    return a + b
+
+print(add(5,5))
+print(add(7,7))
+
+def sub(c,d):
+    return c - d
+
+print(sub(4,4))
+print(sub(9,7))
