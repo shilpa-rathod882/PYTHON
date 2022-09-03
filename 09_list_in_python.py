2c7eedc964b401e85829dbf693e6e521f193d1ec
shilpa-rathod882 <shilpab882@gmail.com>
2022-09-03 14:31:09 +0530

    list methods

---
+++ 09_list_in_python.py
@@ -0,0 +1,19 @@
+lst = []
+lst2 = list()
+
+print(type(lst))
+print(type(lst2))
+
+ele = [1,2,3,2.5,3.5,"Hello"]
+print(type(ele))
+print(ele)
+
+print(ele[0])
+print(ele[-1])
+print(ele[-1][-1])
+
+ele2 = [ele, "World"]
+print(ele2)
+
+print(ele2[0][-1])
+print(ele2[-1])
